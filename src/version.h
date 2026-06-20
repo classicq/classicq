51c388b0a1e48db4112dcd8363c7d5cf1699187b
mg0x7BE <108034506+mg0x7BE@users.noreply.github.com>
2026-06-20 20:58:28 +0200

    Pin release CI build to baseline x86_64 CPU (this time correctly)

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
 #endif
 
 #define	QW_VERSION              2.40
-#define CLASSICQ_VERSION        "3.0.4" COMPILERVERSIONSTRINGAPPEND
+#define CLASSICQ_VERSION        "3.0.5" COMPILERVERSIONSTRINGAPPEND
 
 #if defined(_WIN32) || defined(_WIN64)
 #define QW_PLATFORM     "Windows"
