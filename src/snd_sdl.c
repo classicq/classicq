d7b17f4f908a5547d5e5de497b268b2324254923
mg0x7BE <108034506+mg0x7BE@users.noreply.github.com>
2026-07-14 11:21:00 +0200

    Default s_desiredsamples to 512

--- src/snd_sdl.c
+++ src/snd_sdl.c
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "quakedef.h"
 #include "sound.h"
 
-static cvar_t s_desiredsamples = {"s_desiredsamples", "0", CVAR_ARCHIVE};
+static cvar_t s_desiredsamples = {"s_desiredsamples", "512", CVAR_ARCHIVE};
 
 static SDL_AudioStream *g_stream;
 
